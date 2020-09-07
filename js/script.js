fd7ccfd2211cfd54374db1d266676e4b27ff32f7
Andrei <andrei.dubinko@gmail.com>
2020-09-07 16:35:08 +0300

    redwall test

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let cookie_btn = document.getElementById('cookie-close'),
+    cookies = document.querySelector('.cookies');
+
+cookie_btn.addEventListener('click', function(){
+    cookies.style.display = "none";
+}); 
